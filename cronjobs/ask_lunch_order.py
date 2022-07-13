defe7610afdf70058613e17b195327accec698cc
Daniel Wiedenheft <dwiedenheft@inscribe.com>
2022-07-13 13:48:46 -0400

    Added a newline to the end of a file

--- cronjobs/ask_lunch_order.py
+++ cronjobs/ask_lunch_order.py
@@ -5,3 +5,4 @@ client.run_in_thread()
 
 # Invoke as if someone had said "Hey Mycroft... place lunch order"
 client.emit(Message("recognizer_loop:utterance", {'utterances': ["timed entry order meal"], 'lang': 'en-us'}))
+
